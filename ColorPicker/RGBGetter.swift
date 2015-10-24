79a051dbf521816c0ac990954639d63525693b6a
Laikh Tewari <laikhtewari1@gmail.com>
2015-10-24 03:20:54 -0700

    Function to get RGB values of given point and image

--- ColorPicker/RGBGetter.swift
+++ ColorPicker/RGBGetter.swift
@@ -7,8 +7,25 @@
 //
 
 import Foundation
+import UIKit
 
 class RGBGetter
 {
+    func getRGBAtPointFromImage ( image: UIImage, point: CGPoint) -> (red: Int, green: Int, blue: Int)
+    {
+        let x = point.x
+        let y = point.y
+
+        let pixelData = CGDataProviderCopyData(CGImageGetDataProvider(image.CGImage))
+        let data = CFDataGetBytePtr(pixelData);
+        
+        let pixelInfo = ((image.size.width  * y) + x ) * 4; // The image is png
+        
+        let red = Int(data[Int(pixelInfo)])
+        let green = Int(data[(Int(pixelInfo) + 1)])
+        let blue = Int(data[Int(pixelInfo) + 2])
+        
+        return (red, green, blue)
+    }
     
 }
