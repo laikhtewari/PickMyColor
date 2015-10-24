1e4ba67c8e778a2fe747a43f35bc9757c2ad1349
Laikh Tewari <laikhtewari1@gmail.com>
2015-10-24 02:51:03 -0700

    Created RGBGetter class

---
+++ ColorPicker/RGBGetter.swift
@@ -0,0 +1,14 @@
+//
+//  RGBGetter.swift
+//  ColorPicker
+//
+//  Created by Laikh Tewari on 10/24/15.
+//  Copyright © 2015 Kiran Kunigiri. All rights reserved.
+//
+
+import Foundation
+
+class RGBGetter
+{
+    
+}
