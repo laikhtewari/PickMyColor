e33e163cf4048343fb99c3e40af9373558a85cdb
Kiran <kunigirik@yahoo.com>
2015-10-24 02:29:15 -0400

    Created blank Xcode project

---
+++ ColorPicker/ColorPicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorPicker
+//
+//  Created by Kiran Kunigiri on 10/24/15.
+//  Copyright © 2015 Kiran Kunigiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
